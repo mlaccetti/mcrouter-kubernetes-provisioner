38c081a8e6e6cea19fefb0336326be46b8d61947
Michael Laccetti <michael@laccetti.com>
2017-03-03 08:49:48 -0500

    Updated deps

--- src/main.go
+++ src/main.go
@@ -5,30 +5,54 @@ import (
 	"fmt"
 	"time"
 
-	"k8s.io/client-go/1.4/kubernetes"
-	"k8s.io/client-go/1.4/pkg/api"
-	"k8s.io/client-go/1.4/tools/clientcmd"
+	"k8s.io/client-go/1.5/kubernetes"
+	"k8s.io/client-go/1.5/pkg/api"
+	"k8s.io/client-go/1.5/tools/clientcmd"
 	"os/user"
 	"strings"
+	"k8s.io/client-go/1.5/tools/cache"
+	"k8s.io/kubernetes/pkg/api/v1"
+	"k8s.io/client-go/1.5/pkg/fields"
 )
 
 func main() {
+	// uses the current context in kubeconfig
 	usr, _ := user.Current()
 	dir := usr.HomeDir
 	file := strings.Join([]string{dir, "/.kube/config"}, "")
 
 	kubeconfig := flag.String("kubeconfig", file, "absolute path to the kubeconfig file")
 	flag.Parse()
-	// uses the current context in kubeconfig
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
 		panic(err.Error())
 	}
+
 	// creates the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
+
+	watchlist := cache.NewListWatchFromClient(clientset.CoreClient, "pods", v1.NamespaceDefault,
+		fields.Everything())
+	_, controller := cache.NewInformer(
+		watchlist,
+		&v1.Pod{},
+		time.Second * 0,
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				fmt.Printf("add: %s \n", obj)
+			},
+			DeleteFunc: func(obj interface{}) {
+				fmt.Printf("delete: %s \n", obj)
+			},
+			UpdateFunc:func(oldObj, newObj interface{}) {
+				fmt.Printf("old: %s, new: %s \n", oldObj, newObj)
+			},
+		},
+	)
+
 	for {
 		pods, err := clientset.Core().Pods("").List(api.ListOptions{})
 		if err != nil {
