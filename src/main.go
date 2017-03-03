2d9733efe14ff5878e8f8d1402913831a7ad9404
Michael Laccetti <michael@laccetti.com>
2017-03-03 12:41:09 -0500

    It works!

--- src/main.go
+++ src/main.go
@@ -3,17 +3,50 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os/user"
+	"strings"
 	"time"
 
 	"k8s.io/client-go/1.5/kubernetes"
 	"k8s.io/client-go/1.5/pkg/api"
-	"k8s.io/client-go/1.5/tools/clientcmd"
-	"os/user"
-	"strings"
-	"k8s.io/client-go/1.5/tools/cache"
-	"k8s.io/kubernetes/pkg/api/v1"
+	"k8s.io/client-go/1.5/pkg/api/v1"
 	"k8s.io/client-go/1.5/pkg/fields"
+	"k8s.io/client-go/1.5/pkg/util/wait"
+	"k8s.io/client-go/1.5/tools/cache"
+	"k8s.io/client-go/1.5/tools/clientcmd"
 )
+
+func podCreated(obj interface{}) {
+	pod := obj.(*v1.Pod)
+	fmt.Println("Pod created: "+pod.ObjectMeta.Name)
+}
+
+func podDeleted(obj interface{}) {
+	pod := obj.(*v1.Pod)
+	fmt.Println("Pod deleted: "+pod.ObjectMeta.Name)
+}
+
+func watchPods(client *kubernetes.Clientset) cache.Store {
+	//Define what we want to look for (Pods)
+	watchlist := cache.NewListWatchFromClient(client.CoreClient, "pods", api.NamespaceAll, fields.Everything())
+
+	resyncPeriod := 30 * time.Minute
+
+	//Setup an informer to call functions when the watchlist changes
+	eStore, eController := cache.NewInformer(
+		watchlist,
+		&v1.Pod{},
+		resyncPeriod,
+		cache.ResourceEventHandlerFuncs {
+			AddFunc:    podCreated,
+			DeleteFunc: podDeleted,
+		},
+	)
+
+	//Run the controller as a goroutine
+	go eController.Run(wait.NeverStop)
+	return eStore
+}
 
 func main() {
 	// uses the current context in kubeconfig
@@ -34,31 +67,5 @@ func main() {
 		panic(err.Error())
 	}
 
-	watchlist := cache.NewListWatchFromClient(clientset.CoreClient, "pods", v1.NamespaceDefault,
-		fields.Everything())
-	_, controller := cache.NewInformer(
-		watchlist,
-		&v1.Pod{},
-		time.Second * 0,
-		cache.ResourceEventHandlerFuncs{
-			AddFunc: func(obj interface{}) {
-				fmt.Printf("add: %s \n", obj)
-			},
-			DeleteFunc: func(obj interface{}) {
-				fmt.Printf("delete: %s \n", obj)
-			},
-			UpdateFunc:func(oldObj, newObj interface{}) {
-				fmt.Printf("old: %s, new: %s \n", oldObj, newObj)
-			},
-		},
-	)
-
-	for {
-		pods, err := clientset.Core().Pods("").List(api.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-		time.Sleep(10 * time.Second)
-	}
+	watchPods(clientset)
 }
