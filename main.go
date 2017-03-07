89ab6a68f7c68a5956778184bd5b976fa3894128
Michael Laccetti <michael@laccetti.com>
2017-03-07 12:12:00 -0500

    All the logging
    See if we can figure out what is going on.

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	logger "log"
+	"os"
 	"os/user"
 	"runtime"
 	"strings"
@@ -20,19 +22,25 @@ import (
 	"k8s.io/client-go/1.5/tools/clientcmd"
 )
 
+var log = logger.New(os.Stdout, "[mkp]", logger.Ldate | logger.Ltime | logger.Lmicroseconds | logger.Llongfile)
+
 var clientset *kubernetes.Clientset = nil
 var namespace *string = nil
 var mcrouterConfig *string = nil
 
 func getMemcachedPods() (map[string]string, error) {
+	log.Println("Getting memcached pods from kubernetes.")
+
 	kubeLabelSelector, err := labels.Parse("app in (memcached)")
 
 	if err != nil {
+		log.Fatal("Could not create kubernetes label selector.", err)
 		return nil, err
 	}
 
 	pods, err := clientset.Core().Pods(*namespace).List(api.ListOptions{LabelSelector: kubeLabelSelector})
 	if err != nil {
+		log.Fatal("Could not get memcached pods from kubernetes.", err)
 		return nil, err
 	}
 
@@ -41,15 +49,20 @@ func getMemcachedPods() (map[string]string, error) {
 		memcachedPods[pod.Name] = pod.Status.PodIP
 	}
 
+	log.Println("Retrieved memcached pods from kubernetes.")
 	return memcachedPods, nil
 }
 
-func updateTemplate(pods map[string]string) (error) {
+func updateConfigFile(pods map[string]string) (error) {
+	log.Println("Updating config file.")
+
 	err := lib.Parse("./template/mcrouter-config.tpl", *mcrouterConfig, pods)
 	if err != nil {
+		log.Fatal("Could not create load mcrouter config template.", err)
 		return err
 	}
 
+	log.Println("Config file updated.")
 	return nil
 }
 
@@ -57,21 +70,25 @@ func podsModified(obj interface{}) {
 	pod := obj.(*v1.Pod)
 
 	if pod.ObjectMeta.Labels["app"] == "memcached" {
+		log.Println("Detected pod modification.")
+
 		pods, err := getMemcachedPods()
 		if err != nil {
-			fmt.Println("We could not get a list of memcached pods. ", err)
+			log.Println("We could not get a list of memcached pods. ", err)
 		}
 
-		fmt.Println("memcached pods: ", pods)
+		log.Println("memcached pods: ", pods)
 
-		err = updateTemplate(pods)
+		err = updateConfigFile(pods)
 		if err != nil {
-			fmt.Println("We could not update mcrouter config. ", err)
+			log.Fatal("We could not update mcrouter config. ", err)
 		}
 	}
 }
 
 func watchPods() {
+	log.Println("Configuring pod watcher.")
+
 	//Define what we want to look for (Pods)
 	watchlist := cache.NewListWatchFromClient(clientset.CoreClient, "pods", api.NamespaceAll, fields.Everything())
 
@@ -93,6 +110,8 @@ func watchPods() {
 }
 
 func main() {
+	log.Println("Getting flags...")
+
 	// load our flags
 	inCluster := flag.Bool("incluster", true, "tell us if we are running within kubernetes or not (defaults to true)")
 	namespace = flag.String("namespace", "", "namespace in kubernetes to find memcached pods")
@@ -101,16 +120,29 @@ func main() {
 
 	flag.Parse()
 
+	log.Printf("Processed flags: inCluster [ %b ] | namespace [ %s ] | kubeconfig [ %s ] | mcrouter config [ %s ]\n", inCluster, namespace, kubeconfig, mcrouterConfig)
+
 	if *inCluster {
+		log.Println("Running in-cluster mode, loading config.")
+
 		// in-context
 		// creates the in-cluster config
 		config, err := rest.InClusterConfig()
 		if err != nil {
-			panic(err.Error())
+			log.Panic("Could not load cluster config.", err)
 		}
+
 		// creates the clientset
+		log.Println("Creating in-cluster clientset.")
 		clientset, err = kubernetes.NewForConfig(config)
+
+		if err != nil {
+			log.Panic("Could not create clientset.", err)
+		}
+
+		log.Println("In-cluster clientset created.")
 	} else {
+		log.Println("Running in external mode, checking if user passed in a kubeconfig to use.")
 		if *kubeconfig == "" {
 			// uses the current context in kubeconfig
 			usr, _ := user.Current()
@@ -118,21 +150,26 @@ func main() {
 			*kubeconfig = strings.Join([]string{dir, "/.kube/config"}, "")
 		}
 
+		log.Printf("Using %s as the kubeconfig.", kubeconfig)
+
 		config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 		if err != nil {
-			panic(err.Error())
+			log.Panic("Could not load config.", err)
 		}
 
 		// creates the clientset
+		log.Println("Creating external clientset.")
 		clientset, err = kubernetes.NewForConfig(config)
 		if err != nil {
-			panic(err.Error())
+			log.Panic("Could not create external clientset.", err)
 		}
+
+		log.Println("External clientset created.")
 	}
 
 	watchPods()
 
 	runtime.Goexit()
 
-	fmt.Println("Exit")
+	log.Println("Terminating provisioner.")
 }
