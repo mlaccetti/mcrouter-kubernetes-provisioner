7b701a342961f0ed5d289ca1367b7d46f74e958e
Ian Waters <iwaters@zendesk.com>
2017-06-06 14:49:59 -0700

    more racing

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+while true ; do
+  touch /mcrouter/.foo >> /dev/null 2>&1
+  if [ $? != 0 ]; then
+    echo "/mcrouter isn't available yet"
+    sleep 1;
+  else
+    break;
+  fi
+done
+
 echo "Running: /mcr/bin/mcrouter-kuberentes-provisioner --mcrouterconfig=$MCROUTERCONFIG --inputtemplate=$INPUTTEMPLATE --namespace=$NAMESPACE --incluster=$INCLUSTER --kubeconfig=$KUBECONFIG"
 
 /mcr/bin/mcrouter-kuberentes-provisioner \
