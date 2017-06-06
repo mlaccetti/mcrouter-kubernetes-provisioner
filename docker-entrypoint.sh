0102dd0f217107c52b21b28c79b6a9d5330e4fb6
Ian Waters <iwaters@zendesk.com>
2017-06-06 14:42:15 -0700

    not needed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# wait for the template to avoid possible race
-
-while [ ! -d /mcrouter ]; do
-  echo "waiting for /mcrouter"
-  sleep 10;
-done
-
 echo "Running: /mcr/bin/mcrouter-kuberentes-provisioner --mcrouterconfig=$MCROUTERCONFIG --inputtemplate=$INPUTTEMPLATE --namespace=$NAMESPACE --incluster=$INCLUSTER --kubeconfig=$KUBECONFIG"
 
 /mcr/bin/mcrouter-kuberentes-provisioner \
